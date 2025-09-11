bde6e801000560f2e5639c359d84f2c411311c78
Victor <victorhvercosag@gmail.com>
2025-09-10 22:16:03 -0300

    começando exercício 10

---
+++ ex_10/Program.cs
@@ -0,0 +1,45 @@
+﻿char[] pilhaPalavra = new char[20];
+int posicao = 0;
+
+void InsereFrase(char frase){
+    pilhaPalavra[posicao] = frase;
+    posicao = posicao + 1;
+}
+
+char Remove(){
+    posicao = posicao - 1;
+    return (pilhaPalavra[posicao]);
+}
+
+bool EstaVazia(){
+    if(posicao == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool EstaCheia(){
+    if (posicao == 20){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//principal
+string f;
+Console.Write("Digite uma frase: ");
+f = Console.ReadLine();
+
+//char c = pilhaPalavra[i];
+//int qtd = pilhaPalavra.Length;
+
+InsereFrase(f);
+
+
+while (pilhaPalavra[posicao] != ' '){
+    InsereFrase(f);
+    Remove();
+    
+}
